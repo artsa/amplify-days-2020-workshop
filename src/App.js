73e02e963086f6446ce3600c62085adae0602728
Ari Rantalainen <ari.rantalainen@nordcloud.com>
2020-07-26 09:55:52 +0300

    Frontend added with some customisation and auth

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@
 import React, { useState, useEffect } from 'react';
 
 // import API from Amplify library
-import { API } from 'aws-amplify'
+import { API, Auth } from 'aws-amplify'
 
 // import query definition
 import { listPosts } from './graphql/queries'
 
-export default function App() {
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+export default withAuthenticator(App)
+
+function App() {
   const [posts, setPosts] = useState([])
   useEffect(() => {
     fetchPosts();
+    checkUser(); // new function call
   }, []);
   async function fetchPosts() {
     try {
@@ -20,8 +25,14 @@ export default function App() {
       console.log({ err })
     }
   }
+  async function checkUser() {
+    const user = await Auth.currentAuthenticatedUser();
+    console.log('user:', user);
+    console.log('user attributes: ', user.attributes);
+  }
   return (
     <div>
+      <AmplifySignOut />
       <h1>Hello World</h1>
       {
         posts.map(post => (
